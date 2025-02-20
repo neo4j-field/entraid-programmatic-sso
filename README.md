cf518bef97414079f96169c40acced7a562f9588
yancarlo-perez-neo4j <50329547+yancarlo-perez-neo4j@users.noreply.github.com>
2025-02-20 10:16:09 -0500

    add code

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# entraid-programmatic-sso
-Python example on how to programmatically access neo4j with SSO using Microsoft Entra ID
+# Neo4j SSO with Python & EntraID
+
+This repository is to demonstrate how to connect to a Neo4j database using the python driver via SSO. 
+
+## Installation and Setup
+
+1. Create a `.env` file and fill-in the environment variables as shown in the `example.env`
+
+2. Install the libraries listed in `requirements.txt`.
+```bash
+pip install -r requirements.txt
+```
+
+## Using SSO for a backend Service with EntraID
+In order to allow our python backend to connect to Neo4j using SSO we need to create a technical account (aka Service Principal) that will be used by our script. For this example, we will use a client secret to get a token on behalf of our application. 
+
+This client id & secret are configured in your service principal and we have given it permission in the Neo4j application so it can access our exposed api and get the token to connect. We are also assigning this principal to an existing AD group, Neo4j will use this group membership to determine the authorization this service principal has to query the database.
+
+<img src="images/neo4j_sso_flow.png" width="1000">
---
+++ main.py
@@ -0,0 +1,54 @@
+import os
+import neo4j
+import msal  # Microsoft Authentication Library
+from dotenv import load_dotenv
+from neo4j import bearer_auth
+
+(
+    print(".env variables loaded!")
+    if load_dotenv() else print("Unable to load .env variables.")
+)
+
+# Get information from .env file
+TENANT_ID = os.environ.get("TENANT_ID")
+NEO4J_CLIENT_ID = os.environ.get("NEO4J_CLIENT_ID")
+
+# Service principal credentials
+CLIENT_ID = os.environ.get("CLIENT_ID")
+CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
+
+AUTHORITY = f"https://login.microsoftonline.com/{TENANT_ID}"
+SCOPE = [f"api://{NEO4J_CLIENT_ID}/.default"]
+
+# Neo4j connection uri
+NEO4J_URI = os.environ.get("NEO4J_URI")
+
+
+def get_sso_token():
+    app = msal.ConfidentialClientApplication (
+        CLIENT_ID,
+        authority=AUTHORITY,
+        client_credential=CLIENT_SECRET
+    )
+    # Acquire token silently or via a new request
+    result = app.acquire_token_for_client(scopes=SCOPE)
+
+    if "access_token" in result:
+        return result["access_token"]
+    else:
+        raise Exception(f"Failed to get token: {result.get('error_description')}")
+
+token = get_sso_token()
+#print(token)
+with neo4j.GraphDatabase.driver(
+    NEO4J_URI,
+    auth=bearer_auth(token)
+) as driver:
+    driver.verify_connectivity()
+    records, summary, keys = driver.execute_query(
+        "MATCH (n) RETURN n LIMIT 10", 
+        database_= "neo4j")
+    print("The query `{query}` returned {records_count} records in {time} ms.".format(
+    query=summary.query, records_count=len(records),
+    time=summary.result_available_after
+))
